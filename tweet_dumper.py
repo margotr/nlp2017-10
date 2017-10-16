fbb4a9fdb4fe04ac8f9a69dd7258a6b628b2b55a
margotr <github@margotrutgers.nl>
2017-10-16 14:08:05 +0200

    Pak alle tweets van die nare politici maar

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -1,8 +1,11 @@
+# Code adapted from https://gist.github.com/yanofsky/5436496 for academic use
+
 #!/usr/bin/env python
 # encoding: utf-8
 
 import tweepy  # https://github.com/tweepy/tweepy
 import csv
+import os
 
 # Twitter API credentials
 consumer_key = "e1iHkgum9tHvTuWUtjwqoTOO6"
@@ -11,7 +14,7 @@ access_key = "919862426300043264-YCjEBrp6s5IsvL4RH9zTaKgkTabekzN"
 access_secret = "NOIgJ7yHXi2bU2n3xDLzrTSr8DBw739h2vv3YvDNLNOWa"
 
 
-def get_all_tweets(screen_name):
+def get_all_tweets(screen_name, dir="C:/Users/Margot/PycharmProjects/nlp2017"):
     # Twitter only allows access to a users most recent 3240 tweets with this method
 
     # authorize twitter, initialize tweepy
@@ -49,17 +52,47 @@ def get_all_tweets(screen_name):
         "...%s tweets downloaded so far" % (len(alltweets))
 
     # transform the tweepy tweets into a 2D array that will populate the csv
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+    outtweets = [[tweet.text.encode("utf-8")] for tweet in alltweets]
 
     # write the csv
-    with open('%s_tweets.csv' % screen_name, 'wb') as f:
+    filename = '%s_tweets.csv' % screen_name
+    fullpath = os.path.join(dir, filename)
+    with open(fullpath, 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow(["id", "created_at", "text"])
         writer.writerows(outtweets)
-
     pass
 
 
 if __name__ == '__main__':
     # pass in the username of the account you want to download
     get_all_tweets("J_tsar")
+
+# Do not run this unless you want to reindex all tweets and want to have a coffee break
+def get_labeled_tweets():
+    with open('TwitterAccounts.csv', 'rb') as accounts_csv:
+        reader = csv.reader(accounts_csv)
+        for row in reader:
+            label = row[3]
+
+            filename = '%s_tweets.csv' % row[1]
+            dempath = "C:/Users/Margot/PycharmProjects/nlp2017/rep"
+            reppath = "C:/Users/Margot/PycharmProjects/nlp2017/dem"
+            othpath = "C:/Users/Margot/PycharmProjects/nlp2017/other"
+            dempath_f = os.path.join(dempath, filename)
+            reppath_f = os.path.join(reppath, filename)
+            othpath_f = os.path.join(othpath, filename)
+
+            # Don't try to index tweets we already have
+            if not(os.path.exists(dempath_f) or os.path.exists(reppath_f) or os.path.exists(othpath_f)):
+                if label == 'r':
+                    get_all_tweets(row[1], reppath)
+                elif label == 'd':
+                    get_all_tweets(row[1], dempath)
+                else:
+                    get_all_tweets(row[1], othpath)
+                print "Done: ", row[1], row[3]
+            else:
+                print "Already indexed ", row[1]
+        accounts_csv.close()
+
+get_labeled_tweets()
