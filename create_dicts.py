9c1dd66568a3052d647889dd902f5d798a7be631
Tijmen van Willigen <t.m.vanwilligen@student.utwente.nl>
2017-10-16 14:11:35 +0200

    update

---
+++ create_dicts.py
@@ -0,0 +1,31 @@
+from nltk.tokenize import TweetTokenizer
+from collections import Counter
+
+tokenizer = TweetTokenizer()
+
+def create_ngram_vocabulary(location, n, min):
+    ngrams = []
+
+    tokens = tokenizer.tokenize(location)
+
+    for i, token in enumerate(tokens):
+        print token
+        if n == 1:
+            ngrams.append(token)
+        if n == 2 and tokens[i] != tokens[-1]:
+            ngrams.append(token + tokens[i + 1])
+        if n == 3 and tokens[i] != tokens[-1] and tokens[i] != tokens[-2]:
+            ngrams.append(token, tokens[i + 1], tokens[i + 2])
+        if n == 4 and tokens[i] != tokens[-1] and tokens[i] != tokens[-2] and tokens[i] != tokens[-3]:
+            ngrams.append(token, tokens[i + 1], tokens[i + 2], tokens[i + 3])
+        if n == 5 and tokens[i] != tokens[-1] and tokens[i] != tokens[-2] and tokens[i] != tokens[-3] and tokens[i] != tokens[-4]:
+            ngrams.append(token, tokens[i + 1], tokens[i + 2], tokens[i + 3], token[i + 4])
+
+    d = Counter(ngrams)
+    result = []
+    for k, v in d.items():
+        if v >= min:
+            result.append(k)
+    return result
+
+print create_ngram_vocabulary(open("realdonaldtrump_tweets.csv").read().decode('utf8'), 2, 2)
