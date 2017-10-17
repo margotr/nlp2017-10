e28d761d4592c3a1234bca0ca398835cbc1a8e75
Margot <github@margotrutgers.nl>
2017-10-17 14:58:25 +0200

    Een eerste attempt om te classifying

--- evaluate.py
+++ evaluate.py
@@ -2,11 +2,12 @@ import sys
 import os
 from nltk.tokenize import TweetTokenizer
 import json
+import csv
 
 tokenizer = TweetTokenizer()
 
-def classify_files(dir, predictions_file, eval_file_name, n):
 
+def classify_files(dir, predictions_file, eval_file_name, n):
     # Open ngrams predicrtion file
     with open(predictions_file) as json_file:
         data = json.load(json_file)
@@ -19,13 +20,12 @@ def classify_files(dir, predictions_file, eval_file_name, n):
     # Put the predictions in a dict
     predictions = {}
 
-
     for root, dirs, filenames in os.walk(dir):
         for f in filenames:
-            tokens = tokenizer.tokenize(open(os.path.join(root, f)).read().lower().decode('utf8'))
+            tokens = tokenizer.tokenize(open(os.path.join(root, f)).read().decode('utf8').lower())
             ngrams = []
-            pRep = 0 # For each file, we calculate the probability it is in Rep
-            pDem = 0 # For each file, we calculate the probability it is in Dem
+            pRep = 0  # For each file, we calculate the probability it is in Rep
+            pDem = 0  # For each file, we calculate the probability it is in Dem
             for i, token in enumerate(tokens):
                 if n == 1:
                     ngrams.append(token)
@@ -42,19 +42,56 @@ def classify_files(dir, predictions_file, eval_file_name, n):
             # Find the likelihood for each ngram in the file
             for ngram in ngrams:
                 if ngram in data:
-                    print 'Now doing %s' % ngram
-                    pRep += data[str(ngram)][0]
-                    pDem += data[str(ngram)][1]
+                    try:
+                        print 'Now doing %s' % ngram
+                        pRep += data[str(ngram)][0]
+                        pDem += data[str(ngram)][1]
+                    except:
+                        print ngram, " can't be tested..."
 
             if pRep > pDem:
-                predictions[f] = pRep
+                predictions[f] = "r"
             else:
-                predictions[f] = pDem
+                predictions[f] = "d"
 
     json.dump(predictions, eval_file)
 
 
-classify_files('train_data/dem', 'bigram_min25.txt', 'evaluation.txt', 1)
+classify_files('train_data/rep', 'bigram_min25.txt', 'evaluationREP.txt', 1)
+classify_files('train_data/dem', 'bigram_min25.txt', 'evaluationDEM.txt', 1)
+
+
+def evaluate_scores():
+    demcorrect = 0
+    demwrong = 0
+    repcorrect = 0
+    repwrong = 0
+
+    with open('evaluationDEM.txt') as dem_file:
+        dem_classifications = json.load(dem_file)
+
+    for c in dem_classifications:
+        if dem_classifications[c] == 'd':
+            demcorrect += 1
+        if dem_classifications[c] == 'r':
+            demwrong += 1
+
+    with open('evaluationREP.txt') as rep_file:
+        rep_classifications = json.load(rep_file)
+
+    for c in rep_classifications:
+        if rep_classifications[c] == 'r':
+            repcorrect += 1
+        if rep_classifications[c] == 'd':
+            repwrong += 1
+
+    print "Correctly classified as democrat: ", demcorrect
+    print "Correctly classified as republican: ", repcorrect
+    print "Incorrectly classified as republican: ", demwrong
+    print "Incorrectly classified as democrat ", repwrong
+    print "Total guessed correct: ", (demcorrect + repcorrect), " out of ", (demcorrect + repcorrect + demwrong + repwrong)
+
+evaluate_scores()
 
 
 
